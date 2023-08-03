14fd59cc06d916202dbc8bcdd009e0d42f103db5
Marco Foscato <marco.foscato@uib.no>
2023-08-03 14:07:47 +0200

    warn in case of single-step orca job

--- src/main/java/autocompchem/chemsoftware/orca/OrcaInputWriter.java
+++ src/main/java/autocompchem/chemsoftware/orca/OrcaInputWriter.java
@@ -680,6 +680,18 @@ public class OrcaInputWriter extends ChemSoftInputWriter
 					lines.add(OrcaConstants.JOBSEPARATOR);
 				}
 			}
+    	} else if (job.getNumberOfSteps()==1) {
+    		System.out.println(NL + "WARNING! Found a multistep job with only "
+    				+ "one step. I assume you menat to prepare the input for"
+    				+ "a single step job." + NL);
+    		CompChemJob step = (CompChemJob) job.getStep(0);
+    		preProcessingJob(step);
+    		Iterator<Directive> it = step.directiveIterator();
+			while (it.hasNext())
+			{
+				Directive d = it.next();
+				lines.addAll(getTextForInput(d, true));
+			}
     	} else {
     		preProcessingJob(job);
     		Iterator<Directive> it = job.directiveIterator();
