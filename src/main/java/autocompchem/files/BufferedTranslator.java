3149bec93f40ed218a9e62a601b20c565372cfc0
Marco Foscato <marco.foscato@uib.no>
2024-09-18 00:01:16 +0200

    add buffered reader that translated on the fly

---
+++ src/main/java/autocompchem/files/BufferedTranslator.java
@@ -0,0 +1,311 @@
+package autocompchem.files;
+
+/*
+ *   Copyright (C) 2024  Marco Foscato
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.CharBuffer;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * A buffered reader that translates part of what it reads into something else. 
+ */
+
+public class BufferedTranslator extends BufferedReader
+{
+	/**
+	 * Matches the char sequence that we want to translate into something else
+	 */
+	private Pattern pattern;
+	
+	/**
+	 * The string we want to replace the char sequence with.
+	 */
+	private String replacment;
+	
+	/**
+	 * We must read at least as much as needed to match the regex, and we use 
+	 * the length of the regex as measure of how large the minimal buffer size 
+	 * can be.
+	 */
+	private final int MYBUFFERLENGTH;
+	
+	private final int LENGTHOFREGEX;
+
+	/**
+	 * The buffer of data that has been read from 
+	 * the reader and translated according to the replacement/translation rules.
+	 */
+	private char[] translatedBuffer;
+	
+	/**
+	 * The left over from previous filling of translated buffer. This is the 
+	 * tails of the read-in buffer that could not be translated as it might 
+	 * a portion of any pattern to translate.
+	 */
+	private char[] leftOverBuffer;
+	
+	/**
+	 * The index of the last valid position in the left-over buffer
+	 */
+	private int endLeftOverBuffer = 0;
+	
+	/**
+	 * Flag recording that the reader has reached the end of file
+	 */
+	private boolean reachedEOF = false;
+	
+//------------------------------------------------------------------------------
+
+	/**
+	 * Constructor specifying what to replace and what to replace it with.
+	 * @param in the underlying reader to work with
+	 * @param regexToReplace the REGEX pattern to be replaced.
+	 * @param replacment the string to replace the pattern with.
+	 */
+	
+	public BufferedTranslator(Reader in, String regexToReplace, String replacment) 
+	{
+		this(in, regexToReplace, replacment, 1024);
+	}
+
+//------------------------------------------------------------------------------
+	
+	/**
+	 * Constructor specifying what to replace and what to replace it with.
+	 * @param in the underlying reader to work with
+	 * @param regexToReplace the REGEX pattern to be replaced.
+	 * @param replacment the string to replace the pattern with.
+	 * @param bufferSize the max size of the translated buffer. This will be set 
+	 * to this value only if the given value if larger than three times length
+	 * of regex to match (NB: not that of the match!).
+	 */
+	
+	public BufferedTranslator(Reader in, String regexToReplace, 
+			String replacment, int bufferSize) 
+	{
+		super(in);
+		pattern = Pattern.compile(regexToReplace);
+		this.LENGTHOFREGEX = regexToReplace.length();
+		if (bufferSize > 3*LENGTHOFREGEX)
+			this.MYBUFFERLENGTH = bufferSize;
+		else
+			this.MYBUFFERLENGTH = 1024;
+		this.replacment = replacment;
+	}
+	
+//------------------------------------------------------------------------------
+
+	@Override
+    public int read() throws IOException {
+		//TODO
+		throw new IllegalAccessError("USE OF UNIMPLEMENTED read() in "+this.getClass().getName());
+	}
+
+//------------------------------------------------------------------------------
+
+	@Override
+    public String readLine() throws IOException {
+		String originalLine = super.readLine();
+		if (originalLine==null)
+			return null;
+		
+		Matcher m = pattern.matcher(originalLine);
+		
+		StringBuilder sb = new StringBuilder();
+		while (m.find()) {
+		    m.appendReplacement(sb, replacment);
+		}
+		m.appendTail(sb);
+
+        return sb.toString();
+    }
+
+//------------------------------------------------------------------------------
+	
+	@Override
+	public int read(char[] cbuf, int off, int len) throws IOException 
+	{
+		if (translatedBuffer==null)
+			if (pourToTranslatedBuffer()<1)
+				return -1;
+		
+		// Keep reading and translating as much as we can up to 'len'
+		int returnFromReader = 1; // negative means True
+		while (translatedBuffer.length<len && returnFromReader>0)
+		{
+			returnFromReader = pourToTranslatedBuffer();
+		}
+
+		// Define how much we can write into the destination buffer (cbuf)
+		int numCharsPutInDestBuffer = 0;
+		if (len<=translatedBuffer.length)
+		{
+			numCharsPutInDestBuffer = len; 
+		} else {
+			numCharsPutInDestBuffer = translatedBuffer.length;
+		}
+
+		// Fill the destination buffer
+		for (int i=0; i<numCharsPutInDestBuffer; i++)
+		{
+			cbuf[i] = translatedBuffer[i];
+		}
+		
+		// Consume the translated buffer
+		if (len<=translatedBuffer.length)
+		{
+			translatedBuffer = Arrays.copyOfRange(
+					translatedBuffer, len, translatedBuffer.length);
+		} else {
+			translatedBuffer = null;
+		}
+		
+		return numCharsPutInDestBuffer;
+	}
+	
+//------------------------------------------------------------------------------
+
+	/**
+	 * The logic is to read more than what we actually translate, so that we
+	 * can peek into what comes after the translated piece and
+	 * make sure that the pattern to be matched and translated is not truncated 
+	 * by the end of the buffer.
+	 * @return the number of characters read from the reader, or 
+	 * -1 if the end of file has been reached.
+	 */
+	private int pourToTranslatedBuffer() throws IOException
+	{
+		//TODO del
+		boolean debug = false;
+		if (debug)
+			System.out.println(" ---------------------------------------");
+		
+		char[] readInBuff = new char[MYBUFFERLENGTH];
+		
+		// Take the left over from previous iteration
+		for (int i=0; i<endLeftOverBuffer; i++)
+			readInBuff[i] = leftOverBuffer[i];
+		
+		// tail that we leave as it could contain a fraction of the pattern
+		int tail = 0;
+		
+		// and take more characters from the reader
+		int readInLength = 0;
+		if (!reachedEOF)
+		{
+			readInLength = super.read(readInBuff, endLeftOverBuffer, 
+				MYBUFFERLENGTH-endLeftOverBuffer);
+			tail = LENGTHOFREGEX;
+			// Reached EOF now
+			if (readInLength < 0)
+			{
+				reachedEOF = true;
+				tail = 0;
+				readInLength = 0;
+			}
+		}
+		
+		// Nothing left to poor
+		if (reachedEOF && endLeftOverBuffer<1)
+			return -1;
+		
+		Matcher m = pattern.matcher(CharBuffer.wrap(readInBuff));
+
+		// Find the largest index we can consider without breaking a pattern 
+		// that could be spanning over the limit of the read-in buffer
+		int endOfTranslatable = endLeftOverBuffer + readInLength - tail;
+		int endOfLastMatch = -1;
+		boolean thereIsAnyMatch = false;
+		while (m.find()) {
+			thereIsAnyMatch = true;
+			endOfLastMatch = m.end();
+		}
+		if (thereIsAnyMatch)
+		{
+			endOfTranslatable = Math.max(endOfLastMatch, endOfTranslatable);
+		}
+		
+		//TODO del
+		if (debug)
+		{
+		System.out.println("-FB-: matches?"+thereIsAnyMatch
+				+" endOfTranslatable:"+endOfTranslatable
+				+" readInLength:"+readInLength);
+		System.out.println("-FB-: readInBuff:"+Arrays.toString(readInBuff));
+		}
+		
+		// Translate the section that can be translated
+		char[] toTranslateBuffer = null;
+		if (thereIsAnyMatch)
+		{ 
+			StringBuilder strBuilder = new StringBuilder();
+			m.reset();
+			while (m.find()) {
+			    m.appendReplacement(strBuilder, replacment);
+			}
+			// Do NOT append the tail, as the tail is after 'endOfTranslatable'
+			//m.appendTail(strBuilder)
+			// But we still need to append chars between end of last hit and 
+			// end of translatable. Done here:
+			strBuilder.append(Arrays.copyOfRange(readInBuff, 
+					endOfLastMatch, endOfTranslatable));
+			toTranslateBuffer = new char[strBuilder.length()];
+			strBuilder.getChars(0, strBuilder.length(), toTranslateBuffer, 0);
+		} else {
+			toTranslateBuffer = Arrays.copyOfRange(
+					readInBuff, 0, endOfTranslatable);
+		}
+		if (translatedBuffer!=null)
+		{
+			translatedBuffer = ArrayUtils.addAll(translatedBuffer,
+					toTranslateBuffer);
+		} else {
+			translatedBuffer = toTranslateBuffer;
+		}
+		
+		//TODO del
+		if (debug)
+		{
+		System.out.println("-FB-: translatedBuffer.length:"+translatedBuffer.length);
+		System.out.println("-FB-: translatedBuffer:"+Arrays.toString(translatedBuffer));
+		}
+		
+		// Keep the tail, if any, for next fill iteration
+		leftOverBuffer = Arrays.copyOfRange(readInBuff, endOfTranslatable, 
+				readInLength + endLeftOverBuffer);
+		endLeftOverBuffer = readInLength + endLeftOverBuffer - endOfTranslatable;
+		
+
+		//TODO del
+		if (debug)
+		{
+			System.out.println("-FB-: leftOverBuffer.length:"+leftOverBuffer.length);
+			System.out.println("-FB-: leftOverBuffer:"+Arrays.toString(leftOverBuffer));
+		}
+		return readInLength;
+	}
+	
+//------------------------------------------------------------------------------
+	
+}
---
+++ src/test/java/autocompchem/files/BufferedTranslatorTest.java
@@ -0,0 +1,346 @@
+package autocompchem.files;
+
+/*
+ *   Copyright (C) 2016  Marco Foscato
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import autocompchem.io.IOtools;
+
+
+/**
+ * Unit Test for {@link BufferedTranslator}.
+ * 
+ * @author Marco Foscato
+ */
+
+public class BufferedTranslatorTest 
+{
+    @TempDir 
+    File tempDir;
+    
+    public static String NL = System.getProperty("line.separator");
+    
+//------------------------------------------------------------------------------
+
+    @Test
+    public void testReadline_NoMatch() throws Exception
+    {
+        assertTrue(this.tempDir.isDirectory(),"Should be a directory ");
+        
+        File tmpFile = new File(tempDir.getAbsolutePath() 
+        		+ System.getProperty("file.separator") + "test.txt");
+        IOtools.writeTXTAppend(tmpFile,"1234567890",false);
+        IOtools.writeTXTAppend(tmpFile,"abcdefghij",true);
+        
+        String replacement = "replacement_not_used";
+        BufferedTranslator bt = new BufferedTranslator(new FileReader(tmpFile), 
+        		"patter_that_will_not_be_matched", replacement);
+       
+        String line = null;
+        while ((line = bt.readLine()) != null)
+        {
+        	assertFalse(line.contains(replacement));
+        }
+        bt.close();
+    }
+    
+//------------------------------------------------------------------------------
+
+    @Test
+    public void testReadline_Match() throws Exception
+    {
+        assertTrue(this.tempDir.isDirectory(),"Should be a directory ");
+        
+        File tmpFile = new File(tempDir.getAbsolutePath() 
+        		+ System.getProperty("file.separator") + "test.txt");
+        IOtools.writeTXTAppend(tmpFile,"1234567890",false);
+        IOtools.writeTXTAppend(tmpFile,"abcdefghij_hit1",true);
+        IOtools.writeTXTAppend(tmpFile,"12345hit67890",true);
+        IOtools.writeTXTAppend(tmpFile,"abcdefghij",true);
+        IOtools.writeTXTAppend(tmpFile,"1234567890",true);
+        IOtools.writeTXTAppend(tmpFile,"abcdefghhit3ij",true);
+        
+        String replacement = "replacement_used";
+        BufferedTranslator bt = new BufferedTranslator(new FileReader(tmpFile), 
+        		"h.t[0-9]", replacement);
+       
+        String line = null;
+        int matches = 0;
+        while ((line = bt.readLine()) != null)
+        {
+        	if (line.contains(replacement))
+        		matches++;
+        }
+        bt.close();
+        assertEquals(3, matches);
+    }
+    
+//------------------------------------------------------------------------------
+
+    @Test
+    public void testReadBuffer_NoMatch() throws Exception
+    {
+        assertTrue(this.tempDir.isDirectory(),"Should be a directory ");
+        
+        File tmpFile = new File(tempDir.getAbsolutePath() 
+        		+ System.getProperty("file.separator") + "test.txt");
+        String line1 = "1234567890";  // 10
+        String line2 = "abcdefghij "; // 11
+        String line3 = " 1234567890"; // 11
+        IOtools.writeTXTAppend(tmpFile,line1,false);
+        IOtools.writeTXTAppend(tmpFile,line2,true);
+        IOtools.writeTXTAppend(tmpFile,line3,true);
+        int expectedLength = 35; // 10 + 11 + 11 + (3*newline)
+        
+        String replacement = "replacement_not_used";
+        
+        int[] bufferSize = new int[]{1, 2, 4, 6, 10, 1024};
+        int[] readBuffSize = new int[] {1, 2, 10, 1024};
+        String[] regex = new String[] {"X", "XX", "long_patter_not_found"};
+        
+        for (int i=0; i<bufferSize.length; i++)
+        {
+        	for (int j=0; j<regex.length; j++)
+            {
+        		for (int iRBuff=0; iRBuff<readBuffSize.length; iRBuff++)
+        		{
+			        BufferedTranslator bt = new BufferedTranslator(
+			        		new FileReader(tmpFile), regex[j], replacement, 
+			        		bufferSize[i]);
+			        char[] buffer = new char[readBuffSize[iRBuff]];
+			        StringBuilder sb = new StringBuilder();
+			        int readChars = 0;
+			        while (readChars > -1) 
+			        {
+				        sb.append(Arrays.copyOfRange(buffer, 0, readChars));
+			        	readChars = bt.read(buffer, 0, buffer.length);
+			        }
+			        String whole = sb.toString();
+			        assertTrue(whole.contains(line1));
+			        assertTrue(whole.contains(line2));
+			        assertTrue(whole.contains(line3));
+			        assertFalse(whole.contains(replacement));
+			        assertEquals(expectedLength, whole.length());
+			        bt.close();
+        		}
+            }
+        }
+    }
+    
+//------------------------------------------------------------------------------
+
+    @Test
+    public void testReadBuffer_Match() throws Exception
+    {
+        assertTrue(this.tempDir.isDirectory(),"Should be a directory ");
+        
+        File tmpFile = new File(tempDir.getAbsolutePath() 
+        		+ System.getProperty("file.separator") + "test.txt");
+        List<String> lines = new ArrayList<String>(Arrays.asList(
+        		"hit1234567890",          // 1 match
+        		"abchit2defg_ij ",        // 1 match
+        		" hit2 1234567890",       // 1 match
+        		"abchit2defg_ij hit1",    // 2 matches
+        		"hit2 1234567890 hit0",   // 2 matches
+        		" hit21234567890hit0 ",   // 2 matches
+        		"hit0hit1hit2hit3"));     // 4 matches
+        IOtools.writeTXTAppend(tmpFile,lines,false);
+        
+        String[] replacement = new String[]{"", "@", "@NEWSTR@"};
+        
+        String[] regex = new String[] {"h.t[0-9]", "h", "hi"};
+        
+        // Define expected results
+        int[] hitLength = new int[]{4, 1, 2};
+        int[] numHitsPerLine = new int[]{1, 1, 1, 2, 2, 2, 4};
+        
+        int[] bufferSize = new int[]{1, 2, 4, 6, 10, 1024};
+        int[] readBuffSize = new int[] {1, 2, 10, 1024};
+        for (int iBuff=0; iBuff<bufferSize.length; iBuff++)
+        {
+        	for (int iRegex=0; iRegex<regex.length; iRegex++)
+            {
+            	for (int iRepl=0; iRepl<replacement.length; iRepl++)
+                {
+            		for (int iRBuff=0; iRBuff<readBuffSize.length; iRBuff++)
+            		{
+	            		BufferedTranslator bt = new BufferedTranslator(
+				        		new FileReader(tmpFile), 
+				        		regex[iRegex], replacement[iRepl], 
+				        		bufferSize[iBuff]);
+				        char[] buffer = new char[readBuffSize[iRBuff]];
+				        StringBuilder sb = new StringBuilder();
+				        int readChars = 0;
+				        while (readChars > -1) 
+				        {
+					        sb.append(Arrays.copyOfRange(buffer, 0, readChars));
+				        	readChars = bt.read(buffer, 0, buffer.length);
+				        }
+				        String whole = sb.toString();
+
+				        //NB: the -1 make it keep the trailing empty lines
+				        List<String> translatedLines = new ArrayList<String>(
+				        		Arrays.asList(whole.split("\\r?\\n",-1)));
+				        
+				        // +1 because the writing to file adds a newline char
+				        assertEquals(lines.size()+1, translatedLines.size());
+				        
+				        int expectedTotLenght = 0;
+				        for (int iLine=0; iLine<lines.size(); iLine++)
+				        {
+				        	String msg = "Line:" + iLine
+				        			+ " iRegex:" + iRegex
+				        			+ " iRepl:" + iRepl;
+				        	int expectedLineLength = lines.get(iLine).length() 
+				        			- (hitLength[iRegex] 
+				        					* numHitsPerLine[iLine])
+				        			+ (replacement[iRepl].length() 
+				        					* numHitsPerLine[iLine]);
+				        	assertEquals(expectedLineLength, 
+				        			translatedLines.get(iLine).length(), msg);
+				        	expectedTotLenght = expectedTotLenght 
+				        			+ expectedLineLength 
+				        			+ 1; // counting the newline char
+				        	assertEquals(lines.get(iLine).replaceAll(
+				        			regex[iRegex], replacement[iRepl]),
+				        			translatedLines.get(iLine), msg);
+				        }
+				        assertEquals(expectedTotLenght, whole.length());
+				        bt.close();
+                	}
+                }
+            }
+        }
+    }
+    
+//------------------------------------------------------------------------------
+
+    /*
+     * This is isolated because it tests the case of pattern formed upon first 
+     * replacement of a matched string. These 2nd generation matches must not
+     * be translated.
+     */
+    
+    @Test
+    public void testReadBuffer_Match2() throws Exception
+    {
+        assertTrue(this.tempDir.isDirectory(),"Should be a directory ");
+        
+        File tmpFile = new File(tempDir.getAbsolutePath() 
+        		+ System.getProperty("file.separator") + "test.txt");
+        String line1 = "hhitit";  
+        String line2 = "hhitithhitit"; 
+        IOtools.writeTXTAppend(tmpFile,line1,false);
+        IOtools.writeTXTAppend(tmpFile,line2,true);
+        
+        
+        int[] bufferSize = new int[]{1, 2, 4, 6, 10, 1024};
+        int[] readBuffSize = new int[] {1, 2, 10, 1024};
+        
+        for (int i=0; i<bufferSize.length; i++)
+        {
+    		for (int iRBuff=0; iRBuff<readBuffSize.length; iRBuff++)
+    		{
+		        BufferedTranslator bt = new BufferedTranslator(
+		        		new FileReader(tmpFile), "hit", "", 
+		        		bufferSize[i]);
+		        char[] buffer = new char[readBuffSize[iRBuff]];
+		        StringBuilder sb = new StringBuilder();
+		        int readChars = 0;
+		        while (readChars > -1) 
+		        {
+			        sb.append(Arrays.copyOfRange(buffer, 0, readChars));
+		        	readChars = bt.read(buffer, 0, buffer.length);
+		        }
+		        String whole = sb.toString();
+		        List<String> translatedLines = new ArrayList<String>(
+		        		Arrays.asList(whole.split("\\r?\\n",-1)));
+		        assertEquals(3, translatedLines.size());
+		        assertEquals("hit", translatedLines.get(0));
+		        assertEquals("hithit", translatedLines.get(1));
+		        assertEquals("", translatedLines.get(2));
+		        bt.close();
+    		}
+        }
+    }
+    
+//------------------------------------------------------------------------------
+
+    /*
+     * This is isolated because it tests the case of a regex involving newline
+     * character.
+     */
+    
+    @Test
+    public void testReadBuffer_Match3() throws Exception
+    {
+        assertTrue(this.tempDir.isDirectory(),"Should be a directory ");
+        
+        File tmpFile = new File(tempDir.getAbsolutePath() 
+        		+ System.getProperty("file.separator") + "test.txt");
+        String line1 = "abchit";  
+        String line2 = "hit 123 hit hit"; 
+        IOtools.writeTXTAppend(tmpFile,line1,false);
+        IOtools.writeTXTAppend(tmpFile,line2,true);
+        IOtools.writeTXTAppend(tmpFile,line2,true);
+        IOtools.writeTXTAppend(tmpFile,line2,true);
+        
+        int[] bufferSize = new int[]{1, 2, 4, 6, 10, 1024};
+        int[] readBuffSize = new int[] {1, 2, 10, 1024};
+        
+        for (int i=0; i<bufferSize.length; i++)
+        {
+    		for (int iRBuff=0; iRBuff<readBuffSize.length; iRBuff++)
+    		{
+		        BufferedTranslator bt = new BufferedTranslator(
+		        		new FileReader(tmpFile), "hit\\r?\\nhit", "", 
+		        		bufferSize[i]);
+		        char[] buffer = new char[readBuffSize[iRBuff]];
+		        StringBuilder sb = new StringBuilder();
+		        int readChars = 0;
+		        while (readChars > -1) 
+		        {
+			        sb.append(Arrays.copyOfRange(buffer, 0, readChars));
+		        	readChars = bt.read(buffer, 0, buffer.length);
+		        }
+		        String whole = sb.toString();
+		        List<String> translatedLines = new ArrayList<String>(
+		        		Arrays.asList(whole.split("\\r?\\n",-1)));
+		        assertEquals(2, translatedLines.size());
+		        assertEquals("abc 123 hit  123 hit  123 hit hit", 
+		        		translatedLines.get(0));
+		        assertEquals("", translatedLines.get(1));
+		        bt.close();
+    		}
+        }
+    }
+    
+//------------------------------------------------------------------------------
+
+}
