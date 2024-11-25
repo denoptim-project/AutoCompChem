35f8a059e7e071936dc4935cdb933b59eea8e13a
Marco Foscato <marco.foscato@uib.no>
2024-11-25 15:48:34 +0100

    prevent double spacer in Orca input
    having two spaces in Orca input %maxcore directive triggers error in Orca

--- src/main/java/autocompchem/wiro/chem/orca/OrcaInputWriter.java
+++ src/main/java/autocompchem/wiro/chem/orca/OrcaInputWriter.java
@@ -257,7 +257,7 @@ public class OrcaInputWriter extends ChemSoftInputWriter
 				{
 					line = "%";
 				}
-				line = line + dirName + " ";
+				line = line + dirName;
 				
 				for (Keyword k : d.getAllKeywords())
 				{
