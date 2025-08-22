a3781dccc02f246e2c0705c2008f921880ab66be
Marco Foscato <marco.foscato@uib.no>
2025-08-22 14:10:47 +0200

    update version number

--- src/main/java/autocompchem/ui/ACCMain.java
+++ src/main/java/autocompchem/ui/ACCMain.java
@@ -51,7 +51,7 @@ import autocompchem.worker.WorkerFactory;
 public class ACCMain
 {
     //Software version number
-    private static final String version = "3.1.4";
+    private static final String version = "3.1.5";
     
     // System.spec line separator
     private static final String NL = System.getProperty("line.separator");
