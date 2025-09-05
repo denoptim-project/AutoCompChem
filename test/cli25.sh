84fa4a6063ab31123d14716d0a7fe69a4696354e
Marco Foscato <marco.foscato@uib.no>
2025-09-05 10:53:40 +0200

    run cli25 under a dedicated folder
    tests that use file-matching queries to identify info channels should be run in dedicated folders to avoid listing files from outer tests among the info channels

--- test/cli25.sh
+++ test/cli25.sh
@@ -1,3 +1,5 @@
-"$javaDir/java" -jar "$ACCHome/target/autocompchem-$accVersion-jar-with-dependencies.jar" -t EvaluateJob --joboutputfile ../cli25.out  > cli25.log
+mkdir cli25_wdir
+cp ../cli25.out cli25_wdir
+"$javaDir/java" -jar "$ACCHome/target/autocompchem-$accVersion-jar-with-dependencies.jar" -t EvaluateJob --joboutputfile cli25.out --workdir cli25_wdir  > cli25.log
 
 
