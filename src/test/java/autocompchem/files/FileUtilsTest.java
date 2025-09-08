aad50df1e3573851835434e6c803f71d9039bab1
Marco Foscato <marco.foscato@uib.no>
2025-09-08 15:00:47 +0200

    fix regex compilation in windows

--- src/test/java/autocompchem/files/FileUtilsTest.java
+++ src/test/java/autocompchem/files/FileUtilsTest.java
@@ -116,10 +116,10 @@ sub2_abc/subsub2_abc
    	 */
 		
 	    // NB: the separator is OS-independent (See use of FilenameUtils.separatorsToSystem below)
-		String[] pattern = {".*", ".*[\\\\/].*", ".*", ".*[\\\\/].*", ".*[\\\\/].*[\\\\/].*",
+		String[] pattern = {".*", ".*[/\\\\].*", ".*", ".*[/\\\\].*", ".*[/\\\\].*[/\\\\].*",
 				".*ab.*", ".*ab.*", ".*ab.*", ".*ab.*", ".*ab.*", ".*ab.*",
 				"file.*", "file.*", ".*file.*", ".*file.*", ".*file.*", ".*file.*",
-				"file_abc", ".*[\\\\/]file_abc", ".*[\\\\/]file_abc"};
+				"file_abc", ".*[/\\\\]file_abc", ".*[/\\\\]file_abc"};
 		int[] depth = {1, 2, 1, 2, 3,
 				1, 2, 3, 1, 2, 3,
 				Integer.MAX_VALUE, 2, 3, 1, 2, 3,
@@ -191,8 +191,8 @@ sub2_abc/subsub2_abc
 				+ fileSeparator + ".file_abc"), "text", false);
 		
         assertEquals(6, FileUtils.findByREGEX(tempDir, ".*file_abc", true).size());
-        assertEquals(1, FileUtils.findByREGEX(tempDir, ".*[\\\\/]\\..+le_abc", true).size());
-        assertEquals(5, FileUtils.findByREGEX(tempDir, ".*[\\\\/][^\\.]\\w+le_abc", true).size());
+        assertEquals(1, FileUtils.findByREGEX(tempDir, ".*[/\\\\]\\..+le_abc", true).size());
+        assertEquals(5, FileUtils.findByREGEX(tempDir, ".*[/\\\\][^\\.]\\w+le_abc", true).size());
         assertEquals(2, FileUtils.findByGlob(tempDir, "*file_abc", true).size());
         assertEquals(1, FileUtils.findByGlob(tempDir, "*.file_abc", true).size());
         assertEquals(1, FileUtils.findByGlob(tempDir, "[!.]*ile_abc", true).size());
@@ -203,8 +203,8 @@ sub2_abc/subsub2_abc
 				+ fileSeparator + "ot-her.log"), "text", false);
 
 		assertEquals(12, FileUtils.findByREGEX(tempDir, ".*", false).size());
-		assertEquals(10, FileUtils.findByREGEX(tempDir, ".*[\\\\/][\\w-]+[\\w-\\.]*", false).size());
-		assertEquals(2, FileUtils.findByREGEX(tempDir, ".*[\\\\/]\\.[\\w-]+[\\w-\\.]*", false).size());
+		assertEquals(10, FileUtils.findByREGEX(tempDir, ".*[/\\\\][\\w-]+[\\w-\\.]*", false).size());
+		assertEquals(2, FileUtils.findByREGEX(tempDir, ".*[/\\\\]\\.[\\w-]+[\\w-\\.]*", false).size());
     }
 	
 //------------------------------------------------------------------------------
