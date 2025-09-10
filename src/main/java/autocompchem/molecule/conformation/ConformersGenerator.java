47e64d819d94956a1cc01c641a7e7abe75033e3e
Marco Foscato <marco.foscato@uib.no>
2025-09-10 17:16:08 +0200

    warn about change of atom list upon generation of conformers

--- src/main/java/autocompchem/molecule/conformation/ConformersGenerator.java
+++ src/main/java/autocompchem/molecule/conformation/ConformersGenerator.java
@@ -124,6 +124,7 @@ public class ConformersGenerator extends AtomContainerInputProcessor
     	if (task.equals(GENERATECONFORMERSTASK))
     	{
     		// Reorder atom list to ensure generation of ZMatrix
+    		logger.warn("Reordering atom list. Atom Indexes wil lrefer to the new atom list");
     		MolecularReorderer reorderer = new MolecularReorderer();
     		IAtomContainer orderedIAC = reorderer.reorderContainer(iac);
     		
